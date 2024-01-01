d4e4d94bd0d7a6fe6d1ac1312f8103af20d5ab9f
Adam Jones <106862447+unixthug@users.noreply.github.com>
2024-01-01 16:32:41 -0600

    v2.0
    fixed the bugs with v1.0 and added a word bank

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -1,4 +1,4 @@
-#wordle by Adam Jones for Python | 12/8/2023
+#wordle by Adam Jones for Python 2 | v1.0 12/8/2023 | UPDATED to v2.0 1/1/2024
 
 import random
 
@@ -8,40 +8,57 @@ RED = '\033[41m'
 YELLOW = '\033[43m'
 ENDC = '\033[0m'
 
-#GUESS FUNCTION
+#REWORKED GUESS FUNCTION
 def guess(word, solution):
+	hasY = False
 	value = [ "", "", "", "", "" ]
-	temp = 0
-	if word == solution:
-		print("CORRECT! YOU WIN!")
-		print(GREEN + word + ENDC)
-		exit()
-	for i in range(len(word)):
-		for j in range(len(solution)):
-			if word[i] == solution[j] or word[i] != value[i]:
-				if word[i] == solution[i]:
-					value[i] = GREEN + word[i] + ENDC
-				elif word[i] == solution[j] and temp != j:
-					value[i] = YELLOW + word[i] + ENDC
-					temp = j
-				else:
-					value[i] = RED + word[i] + ENDC
-
+	check = [ "", "", "", "", "" ]
+	tempy = 0
+	for i in range(5):
+		if word[i] == solution[i]:
+			value[i] = GREEN + word[i] + ENDC
+			if hasY:
+				if tempy == i:
+					value[tempyin] = RED + word[tempy] + ENDC
+			check[i] = word[i]
+			continue
+		for j in range(5):
+			if word[i] == solution[j] and word[i] != check[i]:
+				hasY = True
+				value[i] = YELLOW + word[i] + ENDC
+				tempy = j
+				tempyin = i
+				break
+			else:
+				value[i] = RED + word[i] + ENDC
 	for i in value:
-		print(i, end="")
+		print(i,end="")
 	print("")
 	print("Try again.")
 
 #MAIN
-bank = [ "beans", "house", "audio", "pears", "grape", "peony", "apple" ]
-solution = random.choice(bank)
+file = open("wordle_list.txt")
+bank = file.readlines()
+file.close()
+#bank = [ "beans", "house", "audio", "pears", "grape", "peony" ]
+solution = random.choice(bank).strip()
 guesses = 6
 print("Please enter first guess:")
 while True:
-	guess(input(), solution)
-	guesses -= 1
-	print(guesses, end="")
-	print(" guesses left.")
+	word = input()
+	if word == solution:
+		print("CORRECT! YOU WIN!")
+		print(GREEN + word + ENDC)
+		exit()
+	if len(word) == 5:
+		guess(word, solution)
+		guesses -= 1
+		print(guesses,end="")
+		print(" guesses left.")
+	else:
+		print("Hey, that's not 5 letters!")
 	if guesses == 0:
 		print("GAME OVER!")
+		print("The word was: " + solution)
 		exit()
+		
